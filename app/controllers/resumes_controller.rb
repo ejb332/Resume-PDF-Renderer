d7d9f2ab723a09938bdc2395aeb248dbd9dca8a4
Terry Ramsdale <terrams@me.com>
2017-01-09 20:25:39 -0600

    add mime type

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,11 +1,18 @@
 class ResumesController < ApplicationController
-  def index
-    puts 'hello world'
-    render 'index.html.erb'
-  end
+  HEADERS = { 'Authorization' => "Token token=#{ENV['API_KEY']}", 'X-User-Email' => ENV['API_EMAIL'] }
+
+  # def index
+  #   puts 'hello world'
+  #   render 'index.html.erb'
+  # end
 
   def show
-    puts 'hello world'
+    # @student = Unirest.get(
+    #   "#{ENV['API_BASE_URL']}/student/#{params[:id]}",
+    #   headers: HEADERS,
+    # ).body
+
     render 'show.html.erb'
   end
+
 end
